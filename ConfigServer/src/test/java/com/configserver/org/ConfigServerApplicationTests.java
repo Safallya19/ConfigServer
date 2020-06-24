74fee9689d8f67bd4736e8cd0828e8418865b5e5
safalllya <safallya@gmail.com>
2020-06-24 12:41:16 +0530

    config server

---
+++ ConfigServer/src/test/java/com/configserver/org/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
